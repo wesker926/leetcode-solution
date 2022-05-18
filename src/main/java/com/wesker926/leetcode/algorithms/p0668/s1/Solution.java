e13a5cc51593cb91cc630f91ee51fc96094b9700
guohao.wesker <guohao.wesker@bytedance.com>
2022-05-18 14:45:41 +0800

    algorithms:p0668;lcof:;igc:;

--- src/main/java/com/wesker926/leetcode/algorithms/p0668/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p0668/s1/Solution.java
@@ -5,6 +5,9 @@ package com.wesker926.leetcode.algorithms.p0668.s1;
  * @date 2022/5/18
  * @description 二分
  * 规模大无法遍历 -> 转化为二分猜数字
+ * 注意，因为x统计的count为不大于自身的count，因此如果x不在表中，必然存在一个表中数小于x且count相同。
+ * 因此，满足一个count的最小数字必然在表中。
+ * 我们统计left左为count小于k的，因此left必然为count等于k的最小数字，也必然在表中。
  */
 public class Solution {
     public int findKthNumber(int m, int n, int k) {
