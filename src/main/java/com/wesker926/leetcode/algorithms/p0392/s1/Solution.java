0b77688963560a9314292bf890625be48d1826bf
guohao.wesker <guohao.wesker@bytedance.com>
2022-06-16 18:00:31 +0800

    algorithms:p0392;lcof:;igc:;lcof2:;

--- src/main/java/com/wesker926/leetcode/algorithms/p0392/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p0392/s1/Solution.java
@@ -4,6 +4,8 @@ package com.wesker926.leetcode.algorithms.p0392.s1;
  * @author wesker.gh
  * @date 2022/6/16
  * @description 双指针
+ * 如果有非常多的s，可以用dp加速
+ * dp[i]存当前i位置字符的下个字符的位置，dp[i]为字符集大小的数组
  */
 public class Solution {
     public boolean isSubsequence(String s, String t) {
