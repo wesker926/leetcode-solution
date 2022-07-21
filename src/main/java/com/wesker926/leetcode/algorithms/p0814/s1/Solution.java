9d967e53670c53c79dc5dbc07f9aaaf229c93ddf
guohao.wesker <guohao.wesker@bytedance.com>
2022-07-21 10:32:30 +0800

    algorithms:p0814;lcof:;igc:;lcof2:;lcp:;

---
+++ src/main/java/com/wesker926/leetcode/algorithms/p0814/s1/Solution.java
@@ -0,0 +1,39 @@
+package com.wesker926.leetcode.algorithms.p0814.s1;
+
+/**
+ * @author wesker.gh
+ * @date 2022/7/21
+ * @description DFS（后序）
+ */
+public class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        return root.val == 0 && root.left == null && root.right == null ? null : root;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
