cbf2c2c38fbfdaa6cbd42dbe72984ff98de1188d
guohao.wesker <guohao.wesker@bytedance.com>
2022-09-10 15:21:29 +0800

    algorithms:p0669;lcof:;igc:;lcof2:;lcp:;

---
+++ src/main/java/com/wesker926/leetcode/algorithms/p0669/s1/Solution.java
@@ -0,0 +1,44 @@
+package com.wesker926.leetcode.algorithms.p0669.s1;
+
+/**
+ * @author wesker.gh
+ * @date 2022/9/10
+ * @description DFS
+ */
+public class Solution {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        } else if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+
+    private static class TreeNode {
+
+        int val;
+
+        TreeNode left;
+
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
