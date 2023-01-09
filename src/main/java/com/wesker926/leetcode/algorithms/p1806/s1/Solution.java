e633a86c0b06812afc0559f19a556f0ebbae0909
wesker926 <guohao0660@gmail.com>
2023-01-09 11:09:27 +0800

    algorithms:p1806;lcof:;igc:;lcof2:;lcp:;

---
+++ src/main/java/com/wesker926/leetcode/algorithms/p1806/s1/Solution.java
@@ -0,0 +1,34 @@
+package com.wesker926.leetcode.algorithms.p1806.s1;
+
+/**
+ * @author wesker.gh
+ * @date 2023/1/9
+ * @description 数学
+ * 对于第i个元素，如果i < n / 2，则转换后的新坐标为i * 2;否则为i * 2 - (n - 1)；
+ * 不考虑n为2的情况，即为i -> (i * 2) % (n - 1)
+ * 所以问题转换为找到使 (i * 2 ^ k) % (n - 1) == i的最小k，这里两端i可以消除，意味着任意元素成立则所有元素成立
+ * 即找到使(2 ^ k) % (n - 1) == i成立的最小k
+ * 因为n为偶数，因此2 ^ k与n - 1互质，根据欧拉定理，2 ^ f(n - 1) == 1 (mod(n - 1))，其中f为欧拉函数
+ * f(n - 1)一定是一个解，而f(n - 1) <= n - 1，则k <= n - 1
+ * 即找到k的时间复杂度为O(n)
+ */
+public class Solution {
+    public int reinitializePermutation(int n) {
+        if (n == 2) {
+            return 1;
+        }
+        int ans = 0, i = 1;
+        do {
+            i = (i * 2) % (n - 1);
+            ans++;
+        } while (i != 1);
+        return ans;
+
+//        int ans = 0, half = n / 2, i = 1;
+//        do {
+//            i = i < half ? i * 2 : i * 2 - n + 1;
+//            ans++;
+//        } while (i != 1);
+//        return ans;
+    }
+}
