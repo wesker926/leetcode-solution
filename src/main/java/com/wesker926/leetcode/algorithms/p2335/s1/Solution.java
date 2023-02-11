51e541af9c107680a8c921801180abe4483b8699
wesker926 <guohao0660@gmail.com>
2023-02-11 10:42:28 +0800

    algorithms:p2335;lcof:;igc:;lcof2:;lcp:;

---
+++ src/main/java/com/wesker926/leetcode/algorithms/p2335/s1/Solution.java
@@ -0,0 +1,14 @@
+package com.wesker926.leetcode.algorithms.p2335.s1;
+
+/**
+ * @author wesker.gh
+ * @date 2023/2/11
+ * @description 数学
+ * 跟主站-p1753一样
+ */
+public class Solution {
+    public int fillCups(int[] amount) {
+        int max = Math.max(amount[0], Math.max(amount[1], amount[2])), sum = amount[0] + amount[1] + amount[2];
+        return max >= sum - max ? max : (sum + 1) / 2;
+    }
+}
