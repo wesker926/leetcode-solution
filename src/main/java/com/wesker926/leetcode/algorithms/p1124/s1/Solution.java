d18c52238277bcf5a1d79399f846f8a7271c5d0b
wesker926 <guohao0660@gmail.com>
2023-02-14 11:56:10 +0800

    algorithms:p1124;lcof:;igc:;lcof2:;lcp:;

--- src/main/java/com/wesker926/leetcode/algorithms/p1124/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p1124/s1/Solution.java
@@ -4,8 +4,12 @@ package com.wesker926.leetcode.algorithms.p1124.s1;
  * @author wesker.gh
  * @date 2023/2/14
  * @description 贪心（前缀和 + 单调栈）
- * 好题，滑不了。
- * 不同于通常意义的单调栈，这题的栈是先得出，后使用。
+ * 好题，滑不出来。
+ * 本题的前提是前缀和，难点是如何将暴力枚举前缀和优化为线性复杂度。
+ * 本题可转化为寻找 pre[i] - pre[j] > 0，i > j。
+ * 因此对于每个pre[i]，寻找其前方最远的pre[j]即可，可使用单调栈。
+ * 为了避免出栈后还要入栈，本题先得出单调栈，然后倒序遍历pre[i]，这不同于通常意义的单调栈。
+ * 其正确性显而易见。
  */
 public class Solution {
     public int longestWPI(int[] hours) {
--- src/main/java/com/wesker926/leetcode/algorithms/p1124/s2/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p1124/s2/Solution.java
@@ -4,23 +4,25 @@ package com.wesker926.leetcode.algorithms.p1124.s2;
  * @author wesker.gh
  * @date 2023/2/14
  * @description 哈希表
+ * 同样是寻找 pre[i] - pre[j] > 0，i > j。
  * 对于前缀和pre[i]：
  * 如果pre[i]大于0，则本身就是长度为i的满足序列；
- * 如果pre[i]不大于0，且存在比它小的前缀和，则必然存在pre[i]-1的前缀和（因为前缀和是连续变化的）
+ * 如果pre[i]不大于0，且存在比它小的前缀和，则必然存在pre[i]-1的前缀和（因为前缀和是从0开始，连续变化的，参考凹曲线）。
+ * 因此寻找pre[j]可以转化为哈希查找。
  */
 public class Solution {
     public int longestWPI(int[] hours) {
-        int n = hours.length, max = 0;
-        int[] pre = new int[n + 1], hash = new int[10000];
+        int n = hours.length, max = 0, pre = 0;
+        int[] hash = new int[10000];
         for (int i = 1; i <= n; i++) {
-            pre[i] = pre[i - 1] + (hours[i - 1] > 8 ? 1 : -1);
-            if (pre[i] < 0 && hash[pre[i] + 10000] == 0) {
-                hash[pre[i] + 10000] = i;
-            }
-            if (pre[i] > 0) {
+            pre += hours[i - 1] > 8 ? 1 : -1;
+            if (pre > 0) {
                 max = Math.max(max, i);
-            } else if (hash[pre[i] + 9999] != 0) {
-                max = Math.max(max, i - hash[pre[i] + 9999]);
+            } else if (hash[pre + 9999] != 0) {
+                max = Math.max(max, i - hash[pre + 9999]);
+            }
+            if (pre < 0 && hash[pre + 10000] == 0) {
+                hash[pre + 10000] = i;
             }
         }
         return max;
