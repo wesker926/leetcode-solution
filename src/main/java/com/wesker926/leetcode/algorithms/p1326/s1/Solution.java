b505b727b5512e8d1a6c932ad2fb06d2524b70a9
wesker926 <guohao0660@gmail.com>
2023-02-22 16:04:56 +0800

    algorithms:p1326;lcof:;igc:;lcof2:;lcp:;

--- src/main/java/com/wesker926/leetcode/algorithms/p1326/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p1326/s1/Solution.java
@@ -5,12 +5,14 @@ package com.wesker926.leetcode.algorithms.p1326.s1;
  * @date 2023/2/21
  * @description 贪心
  * 这题思路跟主站-p0045（跳跃游戏II）一样
+ * 注意arr[0]是被截断的，因此要用max处理
  */
 public class Solution {
     public int minTaps(int n, int[] ranges) {
         int[] arr = new int[n + 1];
         for (int i = 0; i < n + 1; i++) {
-            arr[Math.max(i - ranges[i], 0)] = Math.min(i + ranges[i], n);
+            int idx = Math.max(i - ranges[i], 0);
+            arr[idx] = Math.max(arr[idx], Math.min(i + ranges[i], n));
         }
         int ans = 0;
         for (int i = 0, cur = 0, nxt; cur < n; ans++, cur = nxt) {
