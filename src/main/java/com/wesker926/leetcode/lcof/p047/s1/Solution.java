56aa7048573f3bd272e5cb13c991fc26ec644298
wesker926 <guohao0660@gmail.com>
2023-03-08 10:14:24 +0800

    algorithms:;lcof:p047;igc:;lcof2:;lcp:;

--- src/main/java/com/wesker926/leetcode/lcof/p047/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/lcof/p047/s1/Solution.java
@@ -7,11 +7,11 @@ package com.wesker926.leetcode.lcof.p047.s1;
  */
 public class Solution {
     public int maxValue(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
+        int n = grid[0].length;
         int[] dp = new int[n + 1];
-        for (int i = 1; i <= m; i++) {
+        for (int[] row : grid) {
             for (int j = 1; j <= n; j++) {
-                dp[j] = Math.max(dp[j], dp[j - 1]) + grid[i - 1][j - 1];
+                dp[j] = Math.max(dp[j], dp[j - 1]) + row[j - 1];
             }
         }
         return dp[n];
