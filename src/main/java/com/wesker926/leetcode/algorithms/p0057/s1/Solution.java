8233b1fbcb765b87c2c0be938ce9ea6a863a9c5e
wesker926 <guohao0660@gmail.com>
2023-08-28 10:17:21 +0800

    algorithms:p0057;lcof:;igc:;lcof2:;lcp:;

--- src/main/java/com/wesker926/leetcode/algorithms/p0057/s1/Solution.java
+++ src/main/java/com/wesker926/leetcode/algorithms/p0057/s1/Solution.java
@@ -10,23 +10,12 @@ import java.util.LinkedList;
 public class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         LinkedList<int[]> ans = new LinkedList<>();
-        int idx = 0;
-        for (; idx < intervals.length && intervals[idx][1] < newInterval[0]; idx++) {
-            ans.add(intervals[idx]);
-        }
-        if (idx == intervals.length || intervals[idx][0] >= newInterval[0]) {
-            ans.add(newInterval);
-        } else {
-            ans.add(intervals[idx]);
-            intervals[idx] = newInterval;
-        }
-        for (; idx < intervals.length; idx++) {
-            if (intervals[idx][0] <= ans.getLast()[1]) {
-                ans.getLast()[1] = Math.max(ans.getLast()[1], intervals[idx][1]);
-            } else {
-                ans.add(intervals[idx]);
-            }
-        }
+        int idx = 0, n = intervals.length;
+        for (; idx < n && intervals[idx][1] < newInterval[0]; idx++) ans.add(intervals[idx]);
+        if (idx < n) newInterval[0] = Math.min(newInterval[0], intervals[idx][0]);
+        for (; idx < n && intervals[idx][0] <= newInterval[1]; idx++)
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
+        for (ans.add(newInterval); idx < n; idx++) ans.add(intervals[idx]);
         return ans.toArray(new int[ans.size()][]);
     }
 }
